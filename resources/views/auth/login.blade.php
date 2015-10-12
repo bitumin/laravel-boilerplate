366554873612bd8d112d2bd4b23f48226740acb0
Mitxel Moriana <moriana.mitxel@gmail.com>
2015-10-12 16:09:14 +0200

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <form method="POST" action="/auth/login">
+                    {!! csrf_field() !!}
+                    <div>
+                        Email
+                        <input type="email" name="email" value="{{ old('email') }}">
+                    </div>
+                    <div>
+                        Password
+                        <input type="password" name="password" id="password">
+                    </div>
+                    <div>
+                        <input type="checkbox" name="remember"> Remember Me
+                    </div>
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
