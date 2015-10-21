3270e2e0afb95dea98e805649697bca42fdd5187
Mitxel Moriana <moriana.mitxel@gmail.com>
2015-10-22 01:52:57 +0200

    updated gmaps examples

--- resources/views/examples/phpgmaps.blade.php
+++ resources/views/examples/phpgmaps.blade.php
@@ -10,10 +10,13 @@
 @endsection
 
 @section('content')
-    {{--GoogleMaps JS and HTML generation--}}
-    {{--Available options and defaults in App\Lib\Phpgmap.php--}}
     <?php
-        //common options for the google map and the static image
+        /*
+         * Google maps embedded map:
+         * Available options and defaults in App\Lib\Phpgmap.php
+         */
+
+        //common options for the embedded google map and the static image
         $radius = 25; //km
         $lat = 41.1451;
         $lng = 25.1242;
@@ -25,7 +28,7 @@
         $width = '640';
         $height = '100';
 
-        //gmap options
+        //custom gmap options
         $map = array();
         $map['map_height'] = '100px';
         $map['map_width'] = '100%';
@@ -42,16 +45,10 @@
         $map['zoomControlPosition'] = 'TOP_RIGHT';
         \Phpgmaps::initialize($map);
 
-        //gmap marker options
-        $marker = array();
-        $marker['position'] = $lat.','.$lng;
-        $marker['icon'] = $markerURL;
-        \Phpgmaps::add_marker($marker);
-
-        //gmap circle options
+        //custom gmap circle options
         $circle = array();
         $circle['center'] = $lat.','.$lng;
-        $circle['radius'] = $radius;
+        $circle['radius'] = $radius*1000; //m
         $circle['strokeColor'] = '#'.$circleBorderColor;
         $circle['strokeOpacity'] = '0.2';
         $circle['strokeWeight'] = '1';
@@ -60,34 +57,125 @@
         $circle['clickable'] = false;
         \Phpgmaps::add_circle($circle);
 
-        //generate the proper code (returns an array with 2 keys: js and html
+        //custom gmap marker options
+        $marker = array();
+        $marker['position'] = $lat.','.$lng;
+        $marker['icon'] = $markerURL;
+        \Phpgmaps::add_marker($marker);
+
+        //generate the proper code (returns an array with 2 keys: js, html
         $GMap =  \Phpgmaps::create_map(); //generate HTML and JS code with the given config options
 
-        //static image
-        $staticImgGMap = \App\Lib\Geo::getGoogleMapsStaticImageURL($width,$height,$lat,$lng,$zoom,true,$markerURL,true,$radius,$circleFillColor,$circleBorderColor);
+        /*
+         * Google maps STATIC IMAGE
+         */
+
+        $staticImgGMap = \App\Lib\Geo::getGoogleMapsStaticImageURL(
+                $width, $height, $lat, $lng, $zoom,
+                true,$markerURL,true,$radius,$circleFillColor,$circleBorderColor
+        );
+
     ?>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 text-center">
+            <div class="col-lg-12">
+                <h2>PHP Google maps (Phpgmaps facade)</h2>
                 {!! $GMap['js'] !!}
                 {!! $GMap['html'] !!}
             </div>
-            <div class="col-lg-6">
-                <div class="well-sm">
+            <div class="col-lg-12">
+                <h3>PHP example</h3>
+                <pre><code class="php">
+                    $radius = 25; //km
+                    $lat = 41.1451;
+                    $lng = 25.1242;
+                    $zoom = 7;
+                    $markerURL = '';
+                    $circleBorderColor = 'd20500';
+                    $circleFillColor = 'd20500';
 
-                </div>
+                    $map = array();
+                    $map['map_height'] = '100px';
+                    $map['map_width'] = '100%';
+                    $map['center'] = $lat.','.$lng;
+                    $map['zoom'] = $zoom;
+                    $map['disableMapTypeControl'] = true;
+                    $map['https'] = false;
+                    $map['disableStreetViewControl'] = true;
+                    $map['disableDoubleClickZoom'] = true;
+                    $map['disableNavigationControl'] = true;
+                    $map['disableScaleControl'] = true;
+                    $map['scrollwheel'] = false;
+                    $map['zoomControlStyle'] = 'SMALL';
+                    $map['zoomControlPosition'] = 'TOP_RIGHT';
+                    \Phpgmaps::initialize($map);
+
+                    $circle = array();
+                    $circle['center'] = $lat.','.$lng;
+                    $circle['radius'] = $radius*1000; //m
+                    $circle['strokeColor'] = '#'.$circleBorderColor;
+                    $circle['strokeOpacity'] = '0.2';
+                    $circle['strokeWeight'] = '1';
+                    $circle['fillColor'] = '#'.$circleFillColor;
+                    $circle['fillOpacity'] = '0.1';
+                    $circle['clickable'] = false;
+                    \Phpgmaps::add_circle($circle);
+
+                    $marker = array();
+                    $marker['position'] = $lat.','.$lng;
+                    $marker['icon'] = $markerURL;
+                    \Phpgmaps::add_marker($marker);
+
+                    $GMap =  \Phpgmaps::create_map();
+                </code></pre>
             </div>
-            <div class="col-lg-6">
-                <div class="well-sm">
-
-                </div>
+            <div class="col-lg-12">
+                <h3>Generated JS</h3>
+                <pre><code class="html">
+                    {{ $GMap['js'] }}
+                </code></pre>
+            </div>
+            <div class="col-lg-12">
+                <h3>Generated HTML</h3>
+                <pre><code class="html">
+                    {{ $GMap['html'] }}
+                </code></pre>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-12" style="
-                height:{{$height}}px;
-                background: url('{{ $staticImgGMap }}') no-repeat;
-            ">
+            <div class="col-lg-12">
+
+            </div>
+            <div class="col-lg-12">
+                <h2>Google maps static image (Geo library method)</h2>
+                <div class="text-center">
+                    <img src="{{ $staticImgGMap }}"/>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <h3>PHP example</h3>
+                <pre><code class="php">
+                    $width = '640';
+                    $height = '100';
+                    $radius = 25; //km
+                    $lat = 41.1451;
+                    $lng = 25.1242;
+                    $zoom = 7;
+                    $markerURL = '';
+                    $circleBorderColor = 'd20500';
+                    $circleFillColor = 'd20500';
+                    $staticImgGMap = \App\Lib\Geo::getGoogleMapsStaticImageURL(
+                            $width, $height, $lat, $lng, $zoom,
+                            $showMarker = true, $markerURL,
+                            $showCircle = true, $radius, $circleFillColor, $circleBorderColor
+                    );
+                </code></pre>
+            </div>
+            <div class="col-lg-12">
+                <h3>Generated string</h3>
+                <pre><code class="html">
+                    {{ $staticImgGMap }}
+                </code></pre>
             </div>
         </div>
     </div>
